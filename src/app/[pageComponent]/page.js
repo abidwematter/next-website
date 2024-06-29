f26a81d83374918dae40b595ca629117f2a672df
abid-sayyed <abidsayyed101@gmail.com>
2024-06-29 17:49:43 +0530

    meta tag working

--- src/app/[pageComponent]/page.js
+++ src/app/[pageComponent]/page.js
@@ -1,26 +1,18 @@
 
 import PageComponent from "@/components/page/pageComponent";
 import { HomeSetting } from "@/services/shared-fetch-service";
-import { Page } from "@/services/shared-fetch-service";
-
-// export async function generateMetadata({ params }) {
-
-//   const homeSetting = await HomeSetting();
-//   const pageData = await Page
-
-//   return {
-//     title: pageData[0]?.meta_title,
-//     keywords: homeSetting[0]?.meta_keyword_home,
-//     description: homeSetting[0]?.meta_description_home,
-//   }
-// }
-
+import { API_CONSTANTS } from "../../shared/constent/API.CONSTENT";
+import HttpFetchService from "@/services/http-fetch-service";
 
 
 export async function generateMetadata({ params }) {
 
+
+  let url = API_CONSTANTS.page.replace("abc", params.pageComponent);
+  const pageData = await HttpFetchService().GET(url);
+
   const homeSetting = await HomeSetting();
-  const pageData = await Page
+
 
   return {
     title: pageData[0]?.meta_title,
